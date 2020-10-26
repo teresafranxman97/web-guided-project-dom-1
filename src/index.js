92fa237dbf533058dd5e5be54a045011f4e079c8
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-26 11:53:01 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const firstCard = document.querySelector(".card");
 const copyOfFirstCard = firstCard.cloneNode(true);
 console.log(copyOfFirstCard);
 
-const cardContainer = null;
+document.querySelector(".card-group")
 
 // A- finding across the entire DOM
 const header = null;
