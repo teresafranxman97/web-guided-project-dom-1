fe666ab7904738ea1a7b6a1f1a3face6c07bdef2
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-26 12:58:36 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -68,6 +68,8 @@ header.style.fontSize = "2em";
 // 👉 6- Creating new elements from scratch and appending them
 // Create a new link inside the nav for "Blog"
 const blogLink = document.createElement("a");
+blogLink.textContent = "Blog";
+
 
 // 👉 7- Making a copy of the card and appending it to the card group
 // DOM nodes can only exist in one spot in the DOM
