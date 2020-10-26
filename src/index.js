33d29337f8f8890a4c4999cdeb5df227eeaeaad7
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-26 12:49:46 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ logoTitle.className = "logo heading banana";
 imageFirstCard.src =
   "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074__340.jpg";
 //  B- Using .setAttribute to change a few attributes
+link1FirstCard.setAttribute()
 
 // 👉 5- Changing the styling of an element
 //  A- By changing the class names on the element with the classList API
