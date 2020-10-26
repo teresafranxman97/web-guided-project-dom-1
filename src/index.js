5f667ef08ba2ca990ea218b825a7a7475c27456d
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-26 11:40:58 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 // A- finding across the entire DOM
 const header = null
 const logoTitle = null
-const firstCard = 
+const firstCard = document.querySelector(".card")
 // B- finding within one particular element
 const imageFirstCard = null
 const titleFirstCard = null
