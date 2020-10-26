b5ba99efc73200f80d5c28d8b1f4fdd0469680b0
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-10-26 12:55:22 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ link2FirstCard.setAttribute("href", "https://doggoipsum.com/");
 //  A- By changing the class names on the element with the classList API
 header.classList.add("sky");
 header.classList.remove("sky");
-setInterval(() => ,1000)
+setInterval(() => header.classList.toggle("sky"), 1000);
 //  B- By manipulating inline styles on the element
 
 // 👉 6- Creating new elements from scratch and appending them
